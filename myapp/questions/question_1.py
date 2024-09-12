fa113ce1c2e9c2022a0c32c623058e935bd3eeaa
Danish <danishjamal525@gmail.com>
2024-09-13 03:26:39 +0530

    first question

---
+++ myapp/questions/question_1.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import time
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+
+@receiver(post_save, sender=MyModel)
+def my_signal_handler(sender, instance, **kwargs):
+    print("Signal handler started")
+    time.sleep(5)  # Simulate a long-running task
+    print("Signal handler finished")
